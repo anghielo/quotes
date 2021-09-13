b6b882a407f946ee21f504464d23164275db885f
anghielo <user_noreply@github.com>
2021-09-13 11:44:25 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/net/floresdany/quotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.floresdany.quotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/quote.dart
@@ -0,0 +1,8 @@
+class Quote {
+
+  String text;
+  String author;
+
+  // Parametized Constructor
+  Quote ({required this.text, required this.author});
+}
